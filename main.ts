8bf57b0fc4e4869172a756a9b166c3c5e1a7713c
ilanatjuuh <77388083+ilanatjuuh@users.noreply.github.com>
2021-01-20 11:22:50 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,28 @@
-music.setVolume(255)
-music.setTempo(120)
-for (let index = 0; index < 2; index++) {
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    for (let index = 0; index < 2; index++) {
-        music.playTone(330, music.beat(BeatFraction.Whole))
-        music.playTone(349, music.beat(BeatFraction.Whole))
-        music.playTone(392, music.beat(BeatFraction.Whole))
-    }
-}
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.Yes)
+    music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
+})
+input.onButtonPressed(Button.A, function () {
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+    basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.Shake, function () {
+    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+    basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+    basic.showIcon(IconNames.SmallHeart)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Fabulous)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+})
+input.onGesture(Gesture.FreeFall, function () {
+    music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+    basic.showIcon(IconNames.No)
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.Once)
+    basic.showIcon(IconNames.House)
+})
